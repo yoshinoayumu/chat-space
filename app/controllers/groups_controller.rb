20f16d1919e7cc9c045e7b085511bdbfb42a77c9
yoshinoayumu <ayumu.yoshino@di-v.co.jp>
2020-05-15 20:32:54 +0900

    メッセージ送信機能
    できた

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -11,19 +11,20 @@ class GroupsController < ApplicationController
   def create
     @group = Group.new(group_params)
     if @group.save
-      redirect_to root_path, notice: 'グループを作成しました'
+      redirect_to group_messages_path, notice: 'グループを作成しました'
     else
       render :new
     end
   end
 
   def edit
+    @group = Group.find(params[:id])
   end
 
   def update
     @group = Group.find(params[:id])
     if @group.update(group_params)
-      redirect_to root_path, notice: 'グループを更新しました'
+      redirect_to group_messages_path(@group), notice: 'グループを更新しました'
     else
       render :edit
     end
